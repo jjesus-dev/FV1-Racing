40a435b15d4773c7e5ee070ba6aa36df06615216
Javier J. Martínez M. <jj_dev@outlook.com>
2025-06-24 18:50:50 -0500

    feat: agrega hud y lógica de puntaje

--- scenes/main.gd
+++ scenes/main.gd
@@ -4,12 +4,27 @@ extends Node
 var score
 
 func game_over() -> void:
+	$ScoreTimer.stop()
 	$Player.hide()
+	$HUD.show_game_over()
 
 func new_game() -> void:
 	score = 0
 	$Player.start($StartPosition.position)
 	$Mob.start($TrampPosition.position)
+	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("¡Preparate!")
 
 func _ready() -> void:
-	new_game()
+	#new_game()
+	pass
+
+
+func _on_score_timer_timeout() -> void:
+	score += 1
+	$HUD.update_score(score)
+
+
+func _on_start_timer_timeout() -> void:
+	$ScoreTimer.start()
