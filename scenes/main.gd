7a3730627acf939e581cacc1f6669c72cb88b700
Javier J. Martínez M. <jj_dev@outlook.com>
2025-07-26 21:50:06 -0600

    feat: modifica lógica de escena principal y jugador

--- scenes/main.gd
+++ scenes/main.gd
@@ -8,10 +8,14 @@ func game_over() -> void:
 	#$Player.hide()
 	$HUD.show_game_over()
 
+func spawn_tramp():
+	var new_tramp = preload("res://scenes/tramp.tscn").instantiate()
+	%PathFollow2D.progress_ratio = 0.3
+	new_tramp.global_position = %PathFollow2D.global_position
+	add_child(new_tramp)
+
 func new_game() -> void:
 	score = 0
-	#$Player.start($StartPosition.position)
-	#$Mob.start($TrampPosition.position)
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("¡Preparate!")
@@ -25,3 +29,9 @@ func _on_score_timer_timeout() -> void:
 
 func _on_start_timer_timeout() -> void:
 	$ScoreTimer.start()
+	$TrampTimer.start()
+
+
+func _on_tramp_timer_timeout() -> void:
+	spawn_tramp()
+	$TrampTimer.wait_time = randf_range(2.5, 6.5)
